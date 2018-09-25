e40a6a4f1f60aa83629f1b36efe5a66dc9018f65
Hady Salama <salama.17@mt330d04.local>
2018-09-25 15:20:05 -0400

    First Working Model. Do not mess with constraints!

--- MMC App (No Scroll)/ViewController.swift
+++ MMC App (No Scroll)/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func digitalSandbox(_ sender: UIButton) {
+        let svc = SFSafariViewController(url: NSURL(string: "http://mmcscholars.com/home/")! as URL)
+         self.present(svc, animated: true, completion: nil)
+    }
+    
+    @IBAction func events(_ sender: UIButton) {
+        let svc = SFSafariViewController(url: NSURL(string: "https://www.google.com/calendar")! as URL)
+         self.present(svc, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func points(_ sender: UIButton) {
+        let svc = SFSafariViewController(url: NSURL(string: "https://www.google.com/sheets")! as URL)
+         self.present(svc, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
