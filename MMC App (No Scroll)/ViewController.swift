15f646a62794dfc422f5d7cca48b955e28932348
Green, Jackie <green.1823@mt330d04.local>
2018-09-25 12:59:03 -0400

    Initial Commit

---
+++ MMC App (No Scroll)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMC App (No Scroll)
+//
+//  Created by Green, Jackie on 9/24/18.
+//  Copyright © 2018 Green, Jackie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
